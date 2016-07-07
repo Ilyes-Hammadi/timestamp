7cd12aa351aeabf8971a085d0d2a21f1410289f5
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2016-07-08 00:01:29 +0100

    fix: Fix the date regex

--- time.js
+++ time.js
@@ -5,7 +5,7 @@ exports.getTimestamp = (time) => {
 
   if(/^\d{8,}$/.test(time)) {
     date = moment(time, 'X')
-  }else if (/^\d+-\d{2,2}-\d{2,2}$/) {
+  }else if (/^\d{4,4}-\d{2,2}-\d{2,2}$/.test(time)) {
     date = moment(time, 'YYYY-M-DD')
   } else {
     date = moment(time, 'MMMM DD, YYYY')
